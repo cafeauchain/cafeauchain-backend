4ca2a3fbed6932d5a1835248da2eed129bd1ab8e
PendragonDevelopment <me@jordankburke.com>
2018-05-08 13:36:47 -0400

    Added service to get initial node address

--- app/services/multichain_services/create_chain_service.rb
+++ app/services/multichain_services/create_chain_service.rb
@@ -1,16 +1,17 @@
 require "faraday"
+require "multichain"
 
 module MultichainServices
   class CreateChainService
     def initialize(farm_slug, network_port, rpc_port)
       @farm_slug = farm_slug
-      @connection = Faraday.new("http://34.234.13.133:5000")
+      @connection = Faraday.new(Rails.application.secrets.host)
       @network_port = network_port
       @rpc_port = rpc_port
     end
 
     def call
-      response = @connection.get("/createchain?chainname=#{@farm_slug}&networkport=#{@network_port}&rpcport=#{@rpc_port}")
+      @connection.get("/createchain?chainname=#{@farm_slug}&networkport=#{@network_port}&rpcport=#{@rpc_port}")
     end
     
     private
