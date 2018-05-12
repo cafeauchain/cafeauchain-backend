753c6b82dca1ed2fd0d5691a9cb17617f355740b
PendragonDevelopment <me@jordankburke.com>
2018-05-11 22:31:27 -0400

    [Finishes #157013840] Added forms and index for farms and chain info

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,46 @@
 # == Route Map
 #
-#                   Prefix Verb   URI Pattern                    Controller#Action
-#         new_user_session GET    /users/sign_in(.:format)       devise/sessions#new
-#             user_session POST   /users/sign_in(.:format)       devise/sessions#create
-#     destroy_user_session DELETE /users/sign_out(.:format)      devise/sessions#destroy
-#        new_user_password GET    /users/password/new(.:format)  devise/passwords#new
-#       edit_user_password GET    /users/password/edit(.:format) devise/passwords#edit
-#            user_password PATCH  /users/password(.:format)      devise/passwords#update
-#                          PUT    /users/password(.:format)      devise/passwords#update
-#                          POST   /users/password(.:format)      devise/passwords#create
-# cancel_user_registration GET    /users/cancel(.:format)        devise/registrations#cancel
-#    new_user_registration GET    /users/sign_up(.:format)       devise/registrations#new
-#   edit_user_registration GET    /users/edit(.:format)          devise/registrations#edit
-#        user_registration PATCH  /users(.:format)               devise/registrations#update
-#                          PUT    /users(.:format)               devise/registrations#update
-#                          DELETE /users(.:format)               devise/registrations#destroy
-#                          POST   /users(.:format)               devise/registrations#create
-#                     page GET    /pages/*id                     high_voltage/pages#show
+#                   Prefix Verb   URI Pattern                          Controller#Action
+#         roaster_profiles GET    /roaster_profiles(.:format)          roaster_profiles#index
+#                          POST   /roaster_profiles(.:format)          roaster_profiles#create
+#      new_roaster_profile GET    /roaster_profiles/new(.:format)      roaster_profiles#new
+#     edit_roaster_profile GET    /roaster_profiles/:id/edit(.:format) roaster_profiles#edit
+#          roaster_profile GET    /roaster_profiles/:id(.:format)      roaster_profiles#show
+#                          PATCH  /roaster_profiles/:id(.:format)      roaster_profiles#update
+#                          PUT    /roaster_profiles/:id(.:format)      roaster_profiles#update
+#                          DELETE /roaster_profiles/:id(.:format)      roaster_profiles#destroy
+#        producer_profiles GET    /producers(.:format)                 producer_profiles#index
+#                          POST   /producers(.:format)                 producer_profiles#create
+#     new_producer_profile GET    /producers/new(.:format)             producer_profiles#new
+#    edit_producer_profile GET    /producers/:id/edit(.:format)        producer_profiles#edit
+#         producer_profile GET    /producers/:id(.:format)             producer_profiles#show
+#                          PATCH  /producers/:id(.:format)             producer_profiles#update
+#                          PUT    /producers/:id(.:format)             producer_profiles#update
+#                          DELETE /producers/:id(.:format)             producer_profiles#destroy
+#         new_user_session GET    /users/sign_in(.:format)             devise/sessions#new
+#             user_session POST   /users/sign_in(.:format)             devise/sessions#create
+#     destroy_user_session DELETE /users/sign_out(.:format)            devise/sessions#destroy
+#        new_user_password GET    /users/password/new(.:format)        devise/passwords#new
+#       edit_user_password GET    /users/password/edit(.:format)       devise/passwords#edit
+#            user_password PATCH  /users/password(.:format)            devise/passwords#update
+#                          PUT    /users/password(.:format)            devise/passwords#update
+#                          POST   /users/password(.:format)            devise/passwords#create
+# cancel_user_registration GET    /users/cancel(.:format)              registrations#cancel
+#    new_user_registration GET    /users/sign_up(.:format)             registrations#new
+#   edit_user_registration GET    /users/edit(.:format)                registrations#edit
+#        user_registration PATCH  /users(.:format)                     registrations#update
+#                          PUT    /users(.:format)                     registrations#update
+#                          DELETE /users(.:format)                     registrations#destroy
+#                          POST   /users(.:format)                     registrations#create
+#                     root GET    /                                    high_voltage/pages#show {:id=>"home"}
+#                     page GET    /*id                                 high_voltage/pages#show
+
 require 'high_voltage'
 
 Rails.application.routes.draw do
 
   resources :roaster_profiles
+  resources :producer_profiles, path: "producers"
   devise_for :users, controllers: { registrations: "registrations" }
 
   root to: 'high_voltage/pages#show', id: 'home'
