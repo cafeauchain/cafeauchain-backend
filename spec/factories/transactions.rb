b5a5706c2e4bf68a5cfa9df5fcabe2afeb7ea5fe
PendragonDevelopment <me@jordankburke.com>
2018-05-13 17:38:25 -0400

    Add transaction

---
+++ spec/factories/transactions.rb
@@ -0,0 +1,27 @@
+# == Schema Information
+#
+# Table name: transactions
+#
+#  id         :bigint(8)        not null, primary key
+#  quantity   :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#  crop_id    :bigint(8)
+#  tx_id      :string
+#
+# Indexes
+#
+#  index_transactions_on_crop_id  (crop_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (crop_id => crops.id)
+#
+
+FactoryBot.define do
+  factory :transaction do
+    crop nil
+    tx_id "MyString"
+    quantity "MyString"
+  end
+end
