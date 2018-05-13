a849b7e860b4fb7b910aeea4e35000b513a78b2e
PendragonDevelopment <me@jordankburke.com>
2018-05-13 17:37:38 -0400

    Added crop

---
+++ app/models/crop.rb
@@ -0,0 +1,26 @@
+# == Schema Information
+#
+# Table name: crops
+#
+#  id                  :bigint(8)        not null, primary key
+#  bag_size            :string
+#  bags                :integer
+#  crop_year           :string
+#  varietal            :string
+#  zone                :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  producer_profile_id :bigint(8)
+#
+# Indexes
+#
+#  index_crops_on_producer_profile_id  (producer_profile_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (producer_profile_id => producer_profiles.id)
+#
+
+class Crop < ApplicationRecord
+  belongs_to :producer_profile
+end
---
+++ spec/factories/crops.rb
@@ -0,0 +1,33 @@
+# == Schema Information
+#
+# Table name: crops
+#
+#  id                  :bigint(8)        not null, primary key
+#  bag_size            :string
+#  bags                :integer
+#  crop_year           :string
+#  varietal            :string
+#  zone                :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  producer_profile_id :bigint(8)
+#
+# Indexes
+#
+#  index_crops_on_producer_profile_id  (producer_profile_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (producer_profile_id => producer_profiles.id)
+#
+
+FactoryBot.define do
+  factory :crop do
+    producer_profile nil
+    crop_year "MyString"
+    zone "MyString"
+    varietal "MyString"
+    bags 1
+    bag_size "MyString"
+  end
+end
