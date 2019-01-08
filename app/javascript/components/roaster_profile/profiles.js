5bc889549521ebb8fcb6cfb60a45cfe41616da57
Kyle Sullivan <ksullivan@roundsphere.com>
2019-01-08 13:03:55 -0500

    Update Roasters component

---
+++ app/javascript/components/roaster_profile/profiles.js
@@ -0,0 +1,46 @@
+import React, { PureComponent, Fragment as F } from "react";
+import PropTypes from "prop-types";
+import { Container, Header, Table, Image } from "semantic-ui-react";
+
+class App extends PureComponent {
+    render() {
+        const { profiles } = this.props;
+        const headerRow = ["Name", "Location", "Website", "Id"];
+        const renderBodyRow = ({ name, city, state, url, id, img_url }) => ({
+            key: id,
+            cells: [
+                {
+                    content: (
+                        <F>
+                            <Image
+                                src={img_url || "https://via.placeholder.com/50?text=No+Image+Available"}
+                                size="mini"
+                                inline
+                                circular={false}
+                                style={{ marginRight: "10px" }}
+                            />
+                            <a href={"roasters/" + id}>{name}</a>
+                        </F>
+                    ),
+                    key: "name"
+                },
+                `${city}, ${state}`,
+                url,
+                id
+            ]
+        });
+        return (
+            <Container className="form roaster-wizard">
+                <Header as="h2">Roasters</Header>
+                <Table striped headerRow={headerRow} renderBodyRow={renderBodyRow} tableData={profiles} />
+            </Container>
+        );
+    }
+}
+
+const { array } = PropTypes;
+App.propTypes = {
+    profiles: array.isRequired
+};
+
+export default App;
