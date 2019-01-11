3a54fff0faa4065e867b6205e38973cb197f76d4
Kyle Sullivan <ksullivan@roundsphere.com>
2019-01-10 23:16:18 -0500

    Actually submitting updates and update loaders

--- app/javascript/components/shared/ImageChange.js
+++ app/javascript/components/shared/ImageChange.js
@@ -1,21 +1,26 @@
-import React, { Component, Fragment } from "react";
+import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Dimmer, Image, Container } from "semantic-ui-react";
+import { Dimmer, Image, Container, Loader } from "semantic-ui-react";
 
 import ImageUpload from "./ImageUpload";
 
 class ImageChange extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            src: props.src,
+            loading: false
+        };
     }
 
     handleShow = () => this.setState({ active: true });
     handleHide = () => this.setState({ active: false });
+    handleImage = ({ src, loading }) => this.setState({ src, loading });
+    handleLoader = ({ loading }, cb) => this.setState({ loading }, cb);
 
     render() {
-        const { active } = this.state;
-        const { src, profile } = this.props;
+        const { active, src, loading } = this.state;
+        const { id } = this.props;
         const inner = (
             <Dimmer
                 active={active}
@@ -23,36 +28,30 @@ class ImageChange extends Component {
                 style={{ padding: 0, background: "rgba(0,0,0,0.6)", border: "1px solid rgba(0,0,0,0.4)" }}
             >
                 <Container style={{ background: "#d9d9d9", padding: "20px 10px" }}>
-                    {/* Once this is working, it should re-render.
-                        Which propoably means that this.props.src should become state.
-                        Also, there probably needs to be a Confirm action/button
-                        so that and the old logo isnt destroyed is the new one gets scrambled
+                    {/* There probably needs to be a Confirm action/button
+                        so that the old logo isnt destroyed if the new one gets scrambled
                         during the upload process */}
-                    <ImageUpload profile={profile} />
+                    <ImageUpload id={id} handleImage={this.handleImage} handleLoader={this.handleLoader} />
                 </Container>
             </Dimmer>
         );
 
         return (
-            <Fragment>
-                <Dimmer.Dimmable
-                    as={Image}
-                    dimmed={active}
-                    onMouseEnter={this.handleShow}
-                    onMouseLeave={this.handleHide}
-                >
-                    <Image src={src} />
-                    {inner}
-                </Dimmer.Dimmable>
-            </Fragment>
+            <Dimmer.Dimmable as={Image} dimmed={active} onMouseEnter={this.handleShow} onMouseLeave={this.handleHide}>
+                <Dimmer active={loading} inverted>
+                    <Loader size="large">Saving</Loader>
+                </Dimmer>
+                <Image src={src} />
+                {inner}
+            </Dimmer.Dimmable>
         );
     }
 }
 
-const { string, object } = PropTypes;
+const { string, number, oneOfType } = PropTypes;
 ImageChange.propTypes = {
     src: string,
-    profile: object
+    id: oneOfType([string, number])
 };
 
 export default ImageChange;
--- app/javascript/components/shared/ImageUpload.js
+++ app/javascript/components/shared/ImageUpload.js
@@ -3,6 +3,7 @@ import PropTypes from "prop-types";
 import { Form } from "semantic-ui-react";
 
 import readCookie from "../utilities/readCookie";
+import API_URL from "../utilities/apiUtils/url";
 
 class LogoUpload extends Component {
     getBase64 = (file, cb) => {
@@ -20,18 +21,20 @@ class LogoUpload extends Component {
     };
 
     uploadFile = event => {
-        this.getBase64(event.target.files[0], result => {
-            this.nextStep({ logo: result });
-        });
+        const { handleLoader } = this.props;
+        handleLoader(
+            { loading: true },
+            this.getBase64(event.target.files[0], result => {
+                this.nextStep({ logo: result });
+            })
+        );
     };
 
     nextStep = async ({ logo }) => {
-        let { profile } = this.props;
-        profile = { ...profile, logo };
-        // TODO Probably just need an UPDATE (PUT) endpoint
-        let url = `/api/v1/roasters/${profile.id}`;
+        let { id, handleImage } = this.props;
+        let url = `${API_URL}/roasters/${id}`;
         const params = {
-            roaster_profile: profile
+            roaster_profile: { logo }
         };
         const token = decodeURIComponent(readCookie("X-CSRF-Token"));
         let response = await fetch(url, {
@@ -45,6 +48,7 @@ class LogoUpload extends Component {
         if (response.status === 200) {
             // eslint-disable-next-line
             console.log(response);
+            setTimeout(() => handleImage({ src: logo, loading: false }), 600);
         } else {
             // eslint-disable-next-line
             console.log("error", response);
@@ -72,8 +76,10 @@ class LogoUpload extends Component {
     }
 }
 
-const { object } = PropTypes;
+const { oneOfType, string, number, func } = PropTypes;
 LogoUpload.propTypes = {
-    profile: object
+    id: oneOfType([string, number]),
+    handleLoader: func,
+    handleImage: func
 };
 export default LogoUpload;
