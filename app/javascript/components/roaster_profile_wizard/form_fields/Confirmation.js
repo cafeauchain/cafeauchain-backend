ce50f7d0d643f1ef69c400f70e8bb4b258d71ddf
PendragonDevelopment <me@jordankburke.com>
2019-01-18 17:18:15 -0500

    ESLint and Prettier changes, plus shortid for uuid in React arrays when unique id doesn't exist

--- app/javascript/components/roaster_profile_wizard/form_fields/Confirmation.js
+++ app/javascript/components/roaster_profile_wizard/form_fields/Confirmation.js
@@ -1,67 +1,76 @@
-import React, { Component } from 'react'
-import { Button, Grid, Icon, Form, Input, List } from 'semantic-ui-react'
-import humanize from '../../utilities/humanize';
-import IconHeader from '../../shared/IconHeader';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import shortid from 'shortid';
+import { Button, Grid, Icon, Form, List } from "semantic-ui-react";
+import humanize from "../../utilities/humanize";
+import IconHeader from "../../shared/IconHeader";
 
 class Confirmation extends Component {
-    state = {}
+    state = {};
 
-    handleChange = (e, { value }) => this.setState({ value })
+    saveAndContinue = e => {
+        const { submitProfile } = this.props
 
-    saveAndContinue = (e) => {
-        e.preventDefault()
-        this.props.submitProfile()
-    }
+        e.preventDefault();
+        submitProfile();
+    };
 
-    goBack = (e) => {
-        e.preventDefault()
-        this.props.previousStep()
-    }
+    goBack = e => {
+        const { previousStep } = this.props
+
+        e.preventDefault();
+        previousStep();
+    };
 
     render() {
-        const {values} = this.props
+        const { values } = this.props;
         return (
             <Grid centered>
                 <Grid.Column width={12}>
                     <Form>
-                        <IconHeader iconName='coffee' header="Confirm your profile info" />
+                        <IconHeader iconName="coffee" header="Confirm your profile info" />
                         <List>
-                            {Object.keys(values).map((key, keyIndex) => 
-                            {
-                                return(
-                                    <List.Item key={keyIndex}>
+                            {Object.keys(values).map((key) => {
+                                return (
+                                    <List.Item key={shortid.generate()}>
                                         <List.Header>{humanize(key)}</List.Header>
                                         {values[key]}
                                     </List.Item>
-                                )
-                            }
-                            )}
+                                );
+                            })}
                         </List>
-                        <Button 
-                            type='submit' 
-                            onClick={this.goBack} 
-                            className="ui left floated" 
-                            icon 
-                            labelPosition='left'
+                        <Button
+                            type="submit"
+                            onClick={this.goBack}
+                            className="ui left floated"
+                            icon
+                            labelPosition="left"
                         >
-                                Previous Step
-                            <Icon name='left arrow' />
+                            Previous Step
+                            <Icon name="left arrow" />
                         </Button>
-                        <Button 
-                            type='submit' 
-                            onClick={this.saveAndContinue} 
-                            className="ui primary right floated" 
-                            icon 
-                            labelPosition='right'
+                        <Button
+                            type="submit"
+                            onClick={this.saveAndContinue}
+                            className="ui primary right floated"
+                            icon
+                            labelPosition="right"
                         >
                             Next Step
-                            <Icon name='right arrow' />
+                            <Icon name="right arrow" />
                         </Button>
                     </Form>
                 </Grid.Column>
             </Grid>
-        )
+        );
     }
 }
+
+const { func, object } = PropTypes;
+Confirmation.propTypes = {
+    submitProfile: func,
+    previousStep: func,
+    values: object
+};
 
 export default Confirmation;
