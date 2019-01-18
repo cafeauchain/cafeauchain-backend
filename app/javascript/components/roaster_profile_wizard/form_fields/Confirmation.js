6ef324059bea64ba82929328d0e25f94d39337e1
PendragonDevelopment <me@jordankburke.com>
2019-01-17 22:29:38 -0500

    ESLinting some indentation, [WIP] Confirmation page, humanize utility

--- app/javascript/components/roaster_profile_wizard/form_fields/Confirmation.js
+++ app/javascript/components/roaster_profile_wizard/form_fields/Confirmation.js
@@ -1,43 +1,67 @@
 import React, { Component } from 'react'
-import { Button, Grid, Icon, Form, Input } from 'semantic-ui-react'
+import { Button, Grid, Icon, Form, Input, List } from 'semantic-ui-react'
+import humanize from '../../utilities/humanize';
 import IconHeader from '../../shared/IconHeader';
 
 class Confirmation extends Component {
-  state = {}
+    state = {}
 
-  handleChange = (e, { value }) => this.setState({ value })
+    handleChange = (e, { value }) => this.setState({ value })
 
-  saveAndContinue = (e) => {
-    e.preventDefault()
-    this.props.submitProfile()
-  }
+    saveAndContinue = (e) => {
+        e.preventDefault()
+        this.props.submitProfile()
+    }
 
-  goBack = (e) => {
-    e.preventDefault()
-    this.props.previousStep()
-  }
+    goBack = (e) => {
+        e.preventDefault()
+        this.props.previousStep()
+    }
 
-  render() {
-    const {value} = this.state
-    return (
-      <Grid centered>
-        <Grid.Column width={12}>
-          <Form>
-            <IconHeader iconName='coffee' header="Confirm your profile info" />
-            
-            <Button type='submit' onClick={this.goBack} className="ui left floated" icon labelPosition='left'>
-              Previous Step
-              <Icon name='left arrow' />
-            </Button>
-            <Button type='submit' onClick={this.saveAndContinue} className="ui primary right floated" icon labelPosition='right'>
-              Next Step
-              <Icon name='right arrow' />
-            </Button>
-          </Form>
-        </Grid.Column>
-      </Grid>
-    )
-  }
+    render() {
+        const {values} = this.props
+        return (
+            <Grid centered>
+                <Grid.Column width={12}>
+                    <Form>
+                        <IconHeader iconName='coffee' header="Confirm your profile info" />
+                        <List>
+                            {Object.keys(values).map((key, keyIndex) => 
+                            {
+                                return(
+                                    <List.Item key={keyIndex}>
+                                        <List.Header>{humanize(key)}</List.Header>
+                                        {values[key]}
+                                    </List.Item>
+                                )
+                            }
+                            )}
+                        </List>
+                        <Button 
+                            type='submit' 
+                            onClick={this.goBack} 
+                            className="ui left floated" 
+                            icon 
+                            labelPosition='left'
+                        >
+                                Previous Step
+                            <Icon name='left arrow' />
+                        </Button>
+                        <Button 
+                            type='submit' 
+                            onClick={this.saveAndContinue} 
+                            className="ui primary right floated" 
+                            icon 
+                            labelPosition='right'
+                        >
+                            Next Step
+                            <Icon name='right arrow' />
+                        </Button>
+                    </Form>
+                </Grid.Column>
+            </Grid>
+        )
+    }
 }
 
 export default Confirmation;
