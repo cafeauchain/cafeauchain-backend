7ac0ff9c43415a82157706f6f9c28691021fcd90
Kyle Sullivan <ksullivan@roundsphere.com>
2019-01-23 18:37:09 -0500

    Functionality for Fee Calculator

--- app/javascript/components/shared/feeCalc/index.js
+++ app/javascript/components/shared/feeCalc/index.js
@@ -3,22 +3,44 @@ import PropTypes from "prop-types";
 import { Container, Input } from "semantic-ui-react";
 
 // This doesn't work but it is what I want to do
-import vars from "stylesheets/variables.scss";
-import "input-range-scss";
+// import vars from "stylesheets/variables.scss";
+// import "input-range-scss";
 
 // This does work but I would prefer to import files directly into the component
 // instead of into the intermediate stylesheet
-import "./styles.scss"
+import "./styles.scss";
 
 class Fees extends Component {
-    state = {};
-    handleInputChange = (event, { value }) => {
-        console.log(event, value);
+    state = {
+        value: 500
     };
+    calculateCost = value => {
+        let cost = "$" + ((value - 500) * 0.02 + 19.99).toFixed(2);
+        return cost;
+    };
+    handleInputChange = (event, { value }) => this.setState({ value });
     render() {
+        const { value } = this.state;
         return (
-            <Container text style={{ background: ( vars && vars.hasOwnProperty( 'text' )) ? vars.text : "brown" }}>
-                <Input type="range" max="10000" min="500" step="100" onChange={this.handleInputChange} />
+            <Container text>
+                <div>
+                    <span>{value}</span>
+                    <span> lb roasted per month</span>
+                </div>
+                <div>
+                    <span>Total Cost: </span>
+                    <span>{this.calculateCost(value)}</span>
+                </div>
+                <Input
+                    type="range"
+                    max="10000"
+                    min="500"
+                    step="100"
+                    onChange={this.handleInputChange}
+                    fluid
+                    className="no-padding"
+                    defaultValue="500"
+                />
             </Container>
         );
     }
