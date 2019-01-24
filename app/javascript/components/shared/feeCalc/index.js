a47e6724e8868b003bfb8e971069d5d09cf59043
Kyle Sullivan <ksullivan@roundsphere.com>
2019-01-24 12:58:06 -0500

    Finalize Fee Calculator Component

--- app/javascript/components/shared/feeCalc/index.js
+++ app/javascript/components/shared/feeCalc/index.js
@@ -1,21 +1,53 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
-import { Container, Input } from "semantic-ui-react";
+import { Container, Input, Header, Card, Statistic, Grid, Image } from "semantic-ui-react";
+import Commafy from "commafy";
 
-// This doesn't work but it is what I want to do
-// import vars from "stylesheets/variables.scss";
-// import "input-range-scss";
+// eslint-disable-next-line
+import img from "images/coffee-imgs/coffee-img-10.jpg";
 
-// This does work but I would prefer to import files directly into the component
-// instead of into the intermediate stylesheet
 import "./styles.scss";
+
+/* eslint-disable */
+const description =
+    "At Cafe au Chain, your cost is completely determined by how much you roast per month. We don't arbitrarily limit the capabilities of the platform based on how big or small of a roaster you are. No limits on employees or accounts or locations. The amount you roast determines the amount you pay. The base package includes up to 500 pounds roasted per month. After that, its $2 per 100 pounds. Use the slider below to estimate your monthly bill.";
+/* eslint-enable */
+
+const FeeGrid = props => {
+    const { poundage, price } = props;
+    return (
+        <Grid divided columns="equal">
+            <Grid.Column textAlign="center">
+                <Statistic value={Commafy(poundage)} label="pounds roasted" />
+            </Grid.Column>
+            <Grid.Column textAlign="center">
+                <Statistic value={<Statistic.Value className="primary-text" content={price} />} label="Total Cost" />
+            </Grid.Column>
+        </Grid>
+    );
+};
+
+const CardImage = () => (
+    <Image
+        floated="right"
+        size="small"
+        src={img}
+        rounded
+        bordered
+        style={{ marginBottom: 20, marginLeft: 60, marginRight: 20 }}
+    />
+);
+
+const RangeInput = props => (
+    <Input {...props} type="range" max="10000" min="500" step="100" fluid className="no-padding" defaultValue="500" />
+);
 
 class Fees extends Component {
     state = {
-        value: 500
+        value: "500"
     };
     calculateCost = value => {
-        let cost = "$" + ((value - 500) * 0.02 + 19.99).toFixed(2);
+        let cost = "$" + ((Number(value) - 500) * 0.02 + 19.99).toFixed(2);
         return cost;
     };
     handleInputChange = (event, { value }) => this.setState({ value });
@@ -23,30 +55,29 @@ class Fees extends Component {
         const { value } = this.state;
         return (
             <Container text>
-                <div>
-                    <span>{value}</span>
-                    <span> lb roasted per month</span>
-                </div>
-                <div>
-                    <span>Total Cost: </span>
-                    <span>{this.calculateCost(value)}</span>
-                </div>
-                <Input
-                    type="range"
-                    max="10000"
-                    min="500"
-                    step="100"
-                    onChange={this.handleInputChange}
-                    fluid
-                    className="no-padding"
-                    defaultValue="500"
-                />
+                <Card centered fluid>
+                    <Card.Content>
+                        <Card.Header>Pricing</Card.Header>
+                        <Card.Description>
+                            <CardImage />
+                            {description}
+                        </Card.Description>
+                    </Card.Content>
+                    <Card.Content extra>
+                        <Header as="h2" content="Monthly Amounts" textAlign="center" block />
+                        <FeeGrid poundage={value} price={this.calculateCost(value)} />
+                    </Card.Content>
+                    <Card.Content>
+                        <RangeInput onChange={this.handleInputChange} />
+                    </Card.Content>
+                </Card>
             </Container>
         );
     }
 }
 const { string } = PropTypes;
-Fees.propTypes = {
-    temp: string
+FeeGrid.propTypes = {
+    price: string,
+    poundage: string
 };
 export default Fees;
