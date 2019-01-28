099fc2479a179d64c76677a296be21628d24b166
Kyle Sullivan <ksullivan@roundsphere.com>
2019-01-28 11:50:21 -0500

    Clean up table component

--- app/javascript/components/shared/table.js
+++ app/javascript/components/shared/table.js
@@ -4,36 +4,64 @@ import PropTypes from "prop-types";
 // import Pagination from "semantic-ui-react-button-pagination";
 import { Pagination, Table } from "semantic-ui-react";
 
+import capitalize from "../utilities/capitalize";
+import namespacer from "../utilities/fieldNamespacer";
+
+// Temporary
+// TODO remove this
+import tableLayout from "../admin/producers/tableDefs";
+
 class FormattedTable extends Component {
     constructor(props) {
         super(props);
         this.state = {};
     }
 
+    buildTableCells = item => {
+        return tableLayout.fields.map(field => {
+            let { namespace, name, ...rest } = field;
+            let value = item[name];
+            if (namespace) {
+                if (typeof namespace === "string") {
+                    value = item[namespace][name];
+                } else {
+                    value = namespacer(namespace, item)[name];
+                }
+            }
+            return (
+                <Table.Cell {...rest} key={field.name}>
+                    {value}
+                </Table.Cell>
+            );
+        });
+    };
+
     render() {
         const { producers, pagination, onPageChange, onClick } = this.props;
         return (
-            <Table celled striped selectable>
+            <Table {...tableLayout.props}>
                 <Table.Header>
                     <Table.Row>
-                        <Table.HeaderCell colSpan="3">Producers</Table.HeaderCell>
+                        <Table.HeaderCell colSpan={tableLayout.fields.length}>{tableLayout.title}</Table.HeaderCell>
+                    </Table.Row>
+                    <Table.Row>
+                        {tableLayout.fields.map(field => (
+                            <Table.HeaderCell key={field.name}>{capitalize(field.name)}</Table.HeaderCell>
+                        ))}
                     </Table.Row>
                 </Table.Header>
 
                 <Table.Body>
-                    {producers.map(producer => {
-                        return (
-                            <Table.Row key={producer.id} onClick={e => onClick(e, producer)}>
-                                <Table.Cell collapsing>{producer.attributes.name}</Table.Cell>
-                                <Table.Cell>{producer.attributes.location}</Table.Cell>
-                            </Table.Row>
-                        );
-                    })}
+                    {producers.map(producer => (
+                        <Table.Row key={producer.id} onClick={e => onClick(e, producer)}>
+                            {this.buildTableCells(producer)}
+                        </Table.Row>
+                    ))}
                 </Table.Body>
 
                 <Table.Footer>
                     <Table.Row>
-                        <Table.HeaderCell colSpan="3">
+                        <Table.HeaderCell colSpan={tableLayout.fields.length} textAlign="right">
                             <Pagination
                                 defaultActivePage={pagination.pagenumber}
                                 totalPages={pagination.totalpages}
