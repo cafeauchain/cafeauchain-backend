ea5d7e0c740efd930bc1bae73f6e704206051b5e
Kyle Sullivan <ksullivan@roundsphere.com>
2019-01-31 11:43:22 -0500

    Update formatters to use commas for numbers

--- app/javascript/components/shared/textFormatters/money.js
+++ app/javascript/components/shared/textFormatters/money.js
@@ -1,29 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-
-import "./styles.scss";
+import AsNumber from "./number";
 
 const Money = props => {
-    let { content, children, className = "", type, decimals = 2 } = props;
-    if (children) content = children;
-    let value = Number(content);
-    className += "text--money";
-    if (type && type !== "neutral") {
-        let direction = "positive";
-        if ((type === "positive" && value < 0) || (type === "negative" && value > 0)) {
-            direction = "negative";
-        }
-        className += " text--" + direction;
-    }
-
-    content = value < 0 ? "-" : "";
-    content += "$" + Math.abs(value).toFixed(decimals);
-
-    return (
-        <span className={className} title={content}>
-            {content}
-        </span>
-    );
+    let { decimals = 2 } = props;
+    return <AsNumber {...props} decimals={decimals} money />;
 };
 
 const { node, string, number } = PropTypes;
---
+++ app/javascript/components/shared/textFormatters/number.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import commafy from "commafy";
+
+import "./styles.scss";
+
+const Comma = props => {
+    let { content, children, className = "", type, decimals, money } = props;
+    if (children) content = children;
+    let value = Number(content);
+    if (type && type !== "neutral") {
+        let direction = "positive";
+        if ((type === "positive" && value < 0) || (type === "negative" && value > 0)) {
+            direction = "negative";
+        }
+        className += " text--" + direction;
+    }
+
+    let result = value < 0 ? "-" : "";
+    result += money ? "$" : "";
+    result += commafy(Math.abs(value).toFixed(decimals));
+
+    return <span className={className}>{result}</span>;
+};
+
+const { node, string, number, bool } = PropTypes;
+Comma.propTypes = {
+    children: node,
+    content: node,
+    className: string,
+    type: string,
+    decimals: number,
+    money: bool
+};
+
+export default Comma;
