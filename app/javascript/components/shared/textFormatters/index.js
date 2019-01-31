c3f92ec4c6f7affdeeb7c1a5bebe920f193603fa
Kyle Sullivan <ksullivan@roundsphere.com>
2019-01-31 12:25:28 -0500

    Add basic time formatter

--- app/javascript/components/shared/textFormatters/index.js
+++ app/javascript/components/shared/textFormatters/index.js
@@ -1,8 +1,9 @@
 import Money from "./money";
 import Truncate from "./truncate";
 import AsNumber from "./number";
+import Time from "./time";
 
 // To pass props to the formatter, the field def should looks something like this:
 //        { name: "id", width: 2, formatter: props => Money({ ...props, type: "negative" }) }
 
-export { Money, Truncate, AsNumber };
+export { Money, Truncate, AsNumber, Time };
