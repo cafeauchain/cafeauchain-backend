e031f2cf4df558d537f2c937d4fc289419847bc4
Kyle Sullivan <ksullivan@roundsphere.com>
2019-02-05 14:31:59 -0500

    Fix calculator if roasted is less than 500

--- app/javascript/components/utilities/roasterCostCalc.js
+++ app/javascript/components/utilities/roasterCostCalc.js
@@ -1,4 +1,7 @@
 const calculateCost = value => {
+    if (Number(value) < 500) {
+        value = 500;
+    }
     let cost = (Math.ceil((Number(value) - 500) / 100) * 2 + 19.99).toFixed(2);
     return cost;
 };
