8db8e13af3d82e74d01565accf99992b08780e07
PendragonDevelopment <me@jordankburke.com>
2019-02-07 10:59:53 -0500

    Updated batch grouping JSON response and annotations

--- app/serializers/crop_serializer.rb
+++ app/serializers/crop_serializer.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: crops
+#
+#  id                  :bigint(8)        not null, primary key
+#  altitude            :string
+#  country             :string
+#  harvest_season      :string
+#  name                :string
+#  process             :string
+#  region              :string
+#  varietal            :string
+#  created_at          :datetime         not null
+#  updated_at          :datetime         not null
+#  producer_profile_id :bigint(8)
+#
+# Indexes
+#
+#  index_crops_on_producer_profile_id  (producer_profile_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (producer_profile_id => producer_profiles.id)
+#
+
 class CropSerializer < ActiveModel::Serializer
   attributes :id, :name, :region, :varietal, :altitude, :producer_profile_id
 end
