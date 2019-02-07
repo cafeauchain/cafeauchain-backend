70f0bebbeffc096abf5d8ed8828ef5d8d4e94555
Kyle Sullivan <ksullivan@roundsphere.com>
2019-02-07 14:48:20 -0500

    Use real data for Budgeter

--- app/javascript/components/roaster_tools/budgeter.js
+++ app/javascript/components/roaster_tools/budgeter.js
@@ -3,16 +3,11 @@ import PropTypes from "prop-types";
 import { Header } from "semantic-ui-react";
 
 /* eslint-disable */
-import getRandomData from "roaster_tools/data/tempTrxByDay";
-
 import { Money, AsNumber } from "shared/textFormatters";
 
 import roasterCostCalc from "utilities/roasterCostCalc";
-import requester from "utilities/apiUtils/requester";
 import API_URL from "utilities/apiUtils/url";
 /* eslint-enable */
-
-// TODO Most of this can probably be deleted when there is real data
 
 class Budgeter extends Component {
     constructor(props) {
@@ -24,21 +19,10 @@ class Budgeter extends Component {
         this.getData(id);
     }
     getData = async id => {
-        let response = await fetch(`/api/v1/roasters/${id}/lots`);
+        let response = await fetch(`${API_URL}/roasters/${id}/lots`);
         const { data } = await response.json();
-        const randomData = getRandomData({
-            start: "2019-02-01",
-            end: "2019-02-28",
-            unit: "day",
-            lots: data.map(lot => lot.id)
-        });
-        this.setState({ total: this.dailyRoastTotals(randomData) });
-    };
-    dailyRoastTotals = data => {
-        return data.reduce((total, date) => {
-            let value = date.amounts.reduce((roasted, amount) => roasted + amount.amount_roasted, 0);
-            return total + value;
-        }, 0);
+        const total = data.reduce((total, amount) => total + amount.attributes.total_amount_roasted, 0);
+        this.setState({ total });
     };
     getRemaining = value => {
         value = Number(value);
