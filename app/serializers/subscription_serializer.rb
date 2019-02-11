a230b13c1fb199c39f94858fc56f709d3dbf0426
PendragonDevelopment <me@jordankburke.com>
2019-02-11 14:04:37 -0500

    Serializer annotated

--- app/serializers/subscription_serializer.rb
+++ app/serializers/subscription_serializer.rb
@@ -1,3 +1,26 @@
+# == Schema Information
+#
+# Table name: subscriptions
+#
+#  id                     :bigint(8)        not null, primary key
+#  next_bill_date         :datetime
+#  status                 :integer
+#  trial_end              :datetime
+#  created_at             :datetime         not null
+#  updated_at             :datetime         not null
+#  stripe_customer_id     :string
+#  stripe_subscription_id :string
+#  user_id                :bigint(8)
+#
+# Indexes
+#
+#  index_subscriptions_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
+
 class SubscriptionSerializer < ActiveModel::Serializer
   attributes :id, :trial_end, :status, :next_bill_date, :amount_roasted_in_cycle, :period_start_date, :period_end_date
 
