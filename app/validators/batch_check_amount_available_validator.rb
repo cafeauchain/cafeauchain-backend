ad45cb5b869198a0fd8c6c7ff8f6f493509c601a
PendragonDevelopment <me@jordankburke.com>
2019-02-19 21:18:31 -0500

    Only validates starting amount on create

--- app/validators/batch_check_amount_available_validator.rb
+++ app/validators/batch_check_amount_available_validator.rb
@@ -1,7 +1,9 @@
 class BatchCheckAmountAvailableValidator < ActiveModel::Validator
   def validate(record)
-    if record.starting_amount.to_f > record.lot.coffee_on_hand.to_f
-      record.errors.add(:starting_amount, "can't be more than coffee on hand")
+    if record.new_record?
+      if record.starting_amount.to_f > record.lot.coffee_on_hand.to_f
+        record.errors.add(:starting_amount, "can't be more than coffee on hand")
+      end
     end
   end
 end
