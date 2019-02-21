8fc81893ebc084d4791eafd4d2a48e3ae0230f24
Kyle Sullivan <ksullivan@roundsphere.com>
2019-02-20 21:01:39 -0500

    Add Item Name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_223908) do
+ActiveRecord::Schema.define(version: 2019_02_21_013211) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 2019_02_20_223908) do
     t.bigint "lot_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["lot_id"], name: "index_inventory_items_on_lot_id"
   end
 
