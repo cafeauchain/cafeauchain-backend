dcfa876b72b0d7aea422f815dd5950850693e4e6
Kyle Sullivan <ksullivan@roundsphere.com>
2019-02-21 17:03:59 -0500

    Allow editing batches

--- app/services/inventory_services/finish_batch_roast.rb
+++ app/services/inventory_services/finish_batch_roast.rb
@@ -1,9 +1,9 @@
 module InventoryServices
   class FinishBatchRoast
-    
-    def self.finish(batch_id, ending_amount)
+
+    def self.finish(batch_id, ending_amount, starting_amount, inventory_item_id)
       @batch = Batch.find(batch_id)
-      @batch.update(ending_amount: ending_amount, status: :roast_completed)
+      @batch.update(ending_amount: ending_amount, status: :roast_completed, starting_amount: starting_amount, inventory_item_id: inventory_item_id)
       return @batch
     end
   end
