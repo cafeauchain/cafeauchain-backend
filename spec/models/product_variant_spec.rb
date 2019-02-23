3ae5893a4d2aa601fad6a1ab196f1dd1d869eca7
PendragonDevelopment <me@jordankburke.com>
2019-02-23 16:38:19 -0500

    Added price to variants

--- spec/models/product_variant_spec.rb
+++ spec/models/product_variant_spec.rb
@@ -4,6 +4,7 @@
 #
 #  id             :bigint(8)        not null, primary key
 #  custom_options :jsonb
+#  price_in_cents :integer
 #  quantity       :integer
 #  variant_title  :string
 #  created_at     :datetime         not null
