ef44e7fbd9681825ccfde50c3f763375a164b94a
PendragonDevelopment <me@jordankburke.com>
2019-02-23 16:36:09 -0500

    Added params snippet to service

--- app/services/inventory_services/create_product.rb
+++ app/services/inventory_services/create_product.rb
@@ -1,5 +1,19 @@
 module InventoryService
   class CreateProduct
+
+    ###########################
+    # Expected params shape:
+    # {
+    #   "name":"Awesome Coffee",
+    #   "description": "Lorem ipsum",
+    #   "product_images":
+    #     [image_file, image_file]
+    #   "composition":
+    #     [{"inventory_item_id":"12","pct":"25"},{"inventory_item_id":"3","pct":"50"},{"inventory_item_id":"11","pct":"25"}],
+    #   "variants":
+    #     [{"size": "12oz", "bean_type": "whole_bean"},{"size": "12oz", "bean_type": "ground"},{"size": "5lb", "bean_type": "whole_bean"}]
+    # }
+    ############################
     
     def initialize(params)
       @product_params = params
