d4c7852c97338375185201bb492c4e1c5dd840c5
Kyle Sullivan <ksullivan@roundsphere.com>
2019-02-24 16:31:21 -0500

    Fix how ArrayToString works

--- app/javascript/components/shared/textFormatters/arrays.js
+++ app/javascript/components/shared/textFormatters/arrays.js
@@ -2,10 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const ArrayToString = props => {
-    let { content, children } = props;
+    let { content, children, keys, output } = props;
     if (children) content = children;
     // eslint-disable-next-line
-    let values = content.map((obj, idx) => <div key={idx + "temp"}>{Object.values(obj).join(": ") + "%"}</div>);
+    let values = content.map((obj, idx) => <div key={idx + "temp"}>{output(keys.map(key => obj[key]))}</div>);
     return values;
 };
 
