90c3180d127a41c4f7501353dec71a056ed2d80e
PendragonDevelopment <me@jordankburke.com>
2019-02-24 20:04:25 -0500

    Updated ids to UUIDs and added indexes

--- db/migrate/20190224001027_create_add_options_to_products.rb
+++
@@ -1,9 +0,0 @@
-class CreateAddOptionsToProducts < ActiveRecord::Migration[5.2]
-  def change
-    create_table :add_options_to_products do |t|
-      t.jsonb :product_options
-
-      t.timestamps
-    end
-  end
-end
