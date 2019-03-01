7a267e0e305c73241a6dd863c44f4f5e2d035458
PendragonDevelopment <me@jordankburke.com>
2019-02-28 20:43:46 -0500

    Added variant options to product serializer

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -23,11 +23,18 @@
 #
 
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :slug, :composition, :product_images, :product_image_urls
+  attributes :id, :title, :description, :slug, :composition, :product_images, :product_image_urls, :variant_options
 
   def composition
     self.object.product_inventory_items.map do |item|
       { name: item.product_name, pct: item.percentage_of_product, inventory_item_id: item.inventory_item.id, id: item.id }
+    end
+  end
+
+  def variant_options
+    object.roaster_profile_id.variant_options.each do |option|
+      # title: string, options: array
+      { title: option.title, options: option.options }
     end
   end
 end
