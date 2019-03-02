a3c4530107141f20134747dc012c2a0519cee6f0
PendragonDevelopment <me@jordankburke.com>
2019-03-02 14:57:50 -0500

    Changed Sentry DSN

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Raven.configure do |config|
-  config.dsn = 'https://787afef10ad642f4aa88c40622b6108f:1d710066b0a74d1daeb0041a7b161f9c@sentry.io/1406268'
+  config.dsn = 'https://787afef10ad642f4aa88c40622b6108f@sentry.io/1406268'
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
   config.environments = ['development', 'production']
 end
