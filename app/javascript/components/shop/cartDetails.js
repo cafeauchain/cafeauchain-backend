5be20f6d2ca86a3fd14fa05dfea03326e970979c
Kyle Sullivan <ksullivan@roundsphere.com>
2019-03-05 15:23:39 -0500

    Remove unused Fragment

--- app/javascript/components/shop/cartDetails.js
+++ app/javascript/components/shop/cartDetails.js
@@ -1,4 +1,4 @@
-import React, { Fragment as F } from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import { Header, Card, Button, Statistic } from "semantic-ui-react";
 
