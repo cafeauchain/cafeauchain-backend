cbf4b3dfcddbb6d4102792e2bbae739f6c810f10
PendragonDevelopment <me@jordankburke.com>
2019-03-07 10:19:34 -0500

    Set up proper method for passing customer profile id

--- app/controllers/api/v1/customers_controller.rb
+++ app/controllers/api/v1/customers_controller.rb
@@ -27,7 +27,7 @@ module Api::V1
     
     def cards
       begin
-        StripeServices::CreateCard.call(nil, params[:customer_profile_id], params[:token], params[:setAsDefault])
+        StripeServices::CreateCard.call(nil, @customer_profile.id, params[:token], params[:setAsDefault])
         render json: @roaster_profile.subscription.cards, status: 200
       rescue StandardError => e
         render json: {
