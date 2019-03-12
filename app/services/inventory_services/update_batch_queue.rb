4722058cb267469f11a8265981bbd4967fcc79d7
Kyle Sullivan <ksullivan@roundsphere.com>
2019-03-11 22:22:38 -0400

    Updating the Product Inventory Item instead of the Variant and Adding to Queue

--- app/services/inventory_services/update_batch_queue.rb
+++ app/services/inventory_services/update_batch_queue.rb
@@ -1,16 +1,12 @@
 module InventoryServices
   class UpdateBatchQueue
-    def self.load_queue(variant)
-      pounds_needed = (variant.custom_options["size"].to_f/16.to_f) * variant.quantity.abs
-      inventory_items = variant.product.product_inventory_items.collect{ |item| [item.inventory_item.id, ((item.percentage_of_product/100) * pounds_needed)] }
-      inventory_items.each do |inventory_item|
-        item = InventoryItem.find(inventory_item[0])
-        if item.batches.where(status: :in_queue).empty?
-          item.batches.create(status: :in_queue, target_weight: inventory_item[1], lot: item.lot, roast_date: (Time.now + 1.day).to_date)
-        else
-          batch = item.batches.where(status: :in_queue).first
-          batch.update(target_weight: (batch.target_weight + inventory_item[1]))
-        end
+    def self.load_queue(inventory_item)
+      pounds_needed = inventory_item.par_level.to_f - inventory_item.quantity.to_f
+      if inventory_item.batches.where(status: :in_queue).empty?
+        inventory_item.batches.create(status: :in_queue, target_weight: pounds_needed, lot: inventory_item.lot, roast_date: (Time.now + 1.day).to_date)
+      else
+        batch = inventory_item.batches.where(status: :in_queue).first
+        batch.update(target_weight: (batch.target_weight.to_f + pounds_needed))
       end
     end
   end
--- app/services/inventory_services/update_variant_inventory.rb
+++ app/services/inventory_services/update_variant_inventory.rb
@@ -1,13 +1,19 @@
+# TODO If this logic is correct, this service probably needs to be renamed
 module InventoryServices
   class UpdateVariantInventory
     def self.update(order)
       order.order_items.each do |order_item|
-        variant = order_item.product_variant
-        variant.update(quantity: (variant.quantity - order_item.quantity))
-        if variant.quantity < 0
-          queue = InventoryServices::UpdateBatchQueue.load_queue(variant)
+        product = order_item.product_variant.product.product_inventory_items.select{ |pii| !pii.inactive }.map do |pii|
+          order_item_weight = order_item.product_variant.custom_options["size"].to_i * order_item.quantity.to_i * pii.percentage_of_product.to_i / 100
+          inventory_item = pii.inventory_item
+          inventory_item_weight = inventory_item.quantity
+          new_ii_weight = inventory_item_weight.to_f - order_item_weight.to_f
+          pii.inventory_item.update( quantity: new_ii_weight)
+          if( inventory_item.quantity.to_f < inventory_item.par_level.to_f)
+            queue = InventoryServices::UpdateBatchQueue.load_queue(inventory_item)
+          end
         end
       end
     end
-  end  
+  end
 end
