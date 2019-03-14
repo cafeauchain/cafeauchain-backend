0651b374d9c3ad1343b2354967f35633f0d969fe
PendragonDevelopment <me@jordankburke.com>
2019-03-14 10:31:32 -0400

    Transaction fixes

--- app/services/inventory_services/create_lot.rb
+++ app/services/inventory_services/create_lot.rb
@@ -9,19 +9,13 @@ module InventoryServices
 
     def call
       @lot = self.lot_creation
-      @batch = self.batch_creation
       LedgerServices::AssetIssueTransaction.new(@params[:lot_size], @crop.id, @roaster.id).call
       LedgerServices::AssetTransferTransaction.new(@params[:on_hand], @lot.id, @roaster.id).call
       LedgerServices::AssetDeliveryTransaction.new(@params[:on_hand], @lot.id, @roaster.id).call
-      LedgerServices::RoastTransaction.new(@params[:roasted], @batch.id, @roaster.id).call
     end
 
     def lot_creation
       @lot = @roaster.lots.create(label: @params[:label], name: @params[:name],harvest_year: @params[:harvest_year], pounds_of_coffee: @params[:lot_size], price_per_pound: @params[:price_per_pound], crop: @crop)
-    end
-
-    def batch_creation
-      @batch = @lot.batches.create(starting_amount: @params[:roasted].to_f, ending_amount: (@params[:roasted].to_f * 0.9))
     end
 
   end
