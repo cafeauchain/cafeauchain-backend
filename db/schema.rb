ef7355bf8d936f5618fc7b9f720f5731e3034472
PendragonDevelopment <me@jordankburke.com>
2019-03-22 16:11:05 -0400

    Added get rates service and way to track shipment id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_21_021443) do
+ActiveRecord::Schema.define(version: 2019_03_22_200925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -213,6 +213,7 @@ ActiveRecord::Schema.define(version: 2019_03_21_021443) do
     t.integer "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "easypost_shipment_id"
     t.index ["wholesale_profile_id"], name: "index_orders_on_wholesale_profile_id"
   end
 
