69eaccffbed1c0f0118743c950e0eeb63d747f40
Kyle Sullivan <ksullivan@roundsphere.com>
2019-03-25 22:32:26 -0400

    Add caps option to humanize

--- app/javascript/components/utilities/humanize.js
+++ app/javascript/components/utilities/humanize.js
@@ -1,8 +1,11 @@
-const humanize = str => {
+const humanize = (str, caps) => {
+    if( caps ){
+        return str.split(/(?=[A-Z])/).join(" ");
+    }
     return str
         .replace(/^[\s_]+|[\s_]+$/g, '')
         .replace(/[_\s]+/g, ' ')
         .replace(/^[a-z]/, function(m) { return m.toUpperCase(); });
-}
+};
 
 export default humanize;
