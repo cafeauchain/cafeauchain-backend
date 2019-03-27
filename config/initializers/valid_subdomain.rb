3cc1fd8f6b838d53133d64c324fa3fa955d3aa62
PendragonDevelopment <me@jordankburke.com>
2019-03-27 15:34:07 -0400

    Swapped route positions

--- config/initializers/valid_subdomain.rb
+++ config/initializers/valid_subdomain.rb
@@ -1,10 +1,8 @@
 class ValidSubdomain
   def self.matches? request
     if RoasterProfile.pluck(:subdomain).include?(request.subdomain)
-      puts RoasterProfile.pluck(:subdomain).include?(request.subdomain)
       return true
     else
-      puts RoasterProfile.pluck(:subdomain).include?(request.subdomain)
       return false
     end
   end
