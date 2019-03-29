ee336c3178b446b9e410d9a102c30404f20d72d1
Kyle Sullivan <ksullivan@roundsphere.com>
2019-03-29 12:23:07 -0400

    WIP Manage Products

---
+++ app/controllers/manage/products_controller.rb
@@ -0,0 +1,35 @@
+module Manage
+  class ProductsController < ApplicationController
+    before_action :set_roaster_profile, only: [:show, :index]
+
+    def index
+      @products = ActiveModel::SerializableResource.new(@roaster.products, each_serializer: ProductSerializer)
+      render "manage/primary", locals: {
+        roaster: @roaster,
+        products: @products,
+        component: "manage/products",
+        title: "Products"
+      }
+    end
+
+    # def show
+    #   @lot = ActiveModel::SerializableResource.new(@lot, serializer: LotSerializer::SingleLotSerializer)
+    #   render "manage/primary", locals: {
+    #     roaster: @roaster,
+    #     lot: @lot,
+    #     component: "lots/single",
+    #     title: "View Lot"
+    #   }
+    # end
+
+    private
+
+    def set_roaster_profile
+      @roaster = current_user.roaster_profile
+    end
+
+    def set_lot
+      @lot = Lot.find(params[:id])
+    end
+  end
+end
