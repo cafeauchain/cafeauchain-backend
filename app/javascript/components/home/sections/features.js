53578c9f1fcac714f977c2582b2b55b6a884be0c
Kyle Sullivan <ksullivan@roundsphere.com>
2019-03-29 16:13:14 -0400

    Rebuilt the homepage

--- app/javascript/components/home/sections/features.js
+++ app/javascript/components/home/sections/features.js
@@ -4,10 +4,10 @@ import { Container, Header, Button, Divider } from "semantic-ui-react";
 const Features = () => {
     return (
         <Container text>
-            <Header as="h3" style={{ fontSize: "2em" }}>
+            <Header as="h3" className="home-section-header">
                 Do Customers Care About Your Coffee&apos;s Sustainability?
             </Header>
-            <p style={{ fontSize: "1.33em" }}>
+            <p className="home-section-text">
                 All participants in the coffee supply chain care about sustainability - 
                 it&apos;s vital for their livelihood. But do your customers actually care?
                 What drives their decisions to buy from you?
@@ -20,10 +20,10 @@ const Features = () => {
                 <a href="/">Case Studies</a>
             </Divider>
 
-            <Header as="h3" style={{ fontSize: "2em" }}>
+            <Header as="h3" className="home-section-header">
                 Coffee Harvesting and the Role of an Aging Farmer Population
             </Header>
-            <p style={{ fontSize: "1.33em" }}>
+            <p className="home-section-text">
                 With coffee farmers getting older and shrinking wages, the median age of coffee farmers is increasing 
                 to where there will potentially be a shortage of workers in the next few decades.
             </p>
