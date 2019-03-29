0824bd31b0ff6477f82b48e2b9839230bf2da4f1
Kyle Sullivan <ksullivan@roundsphere.com>
2019-03-29 15:43:25 -0400

    Rework home header

--- app/javascript/components/home/sections/hero.js
+++ app/javascript/components/home/sections/hero.js
@@ -1,41 +1,28 @@
-import React, { Fragment as F } from "react";
+import React from "react";
 import { Header, Button, Icon } from "semantic-ui-react";
-import PropTypes from "prop-types";
 
-const Hero = ({ mobile }) => {
+const Hero = () => {
     return (
-        <F>
+        <div className="home-hero-container">
             <Header
                 as="h1"
                 content="Cafe au Chain"
                 inverted
-                style={{
-                    fontSize: mobile ? "2em" : "4em",
-                    fontWeight: "normal",
-                    marginBottom: 0
-                }}
+                className="home-hero-main"
             />
             <Header
                 as="h2"
                 content="Proof of Perk"
                 inverted
-                style={{
-                    fontSize: mobile ? "1.7em" : "3.5em",
-                    fontWeight: "normal",
-                    marginBottom: 0
-                }}
+                className="home-hero-sub"
             />
             <Header
                 as="h3"
                 content="Start your journey!"
                 inverted
-                style={{
-                    fontSize: mobile ? "1.5em" : "1.7em",
-                    fontWeight: "normal",
-                    marginTop: mobile ? "0.5em" : "1.5em"
-                }}
+                className="home-hero-tertiary"
             />
-            <p>
+            <p className="home-hero-text">
                 Proof of Perk provides a set of blockchain-backed roaster tools to help bring transparency to the
                 specitiality coffee supply chain.
             </p>
@@ -43,11 +30,8 @@ const Hero = ({ mobile }) => {
                 Get Started
                 <Icon name="right arrow" />
             </Button>
-        </F>
+        </div>
     );
 };
-const { bool } = PropTypes;
-Hero.propTypes = {
-    mobile: bool
-};
+
 export default Hero;
