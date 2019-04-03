9d21f449ffad0819b16ffef548c9cfbd45c34dd3
Kyle Sullivan <ksullivan@roundsphere.com>
2019-04-03 17:25:59 -0400

    Fix Customer Cart - Issue #47

--- app/javascript/components/shop/cart.js
+++ app/javascript/components/shop/cart.js
@@ -1,52 +1,27 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Segment, Item, Sticky, Header, Rail } from "semantic-ui-react";
+import { Segment, Item, Header } from "semantic-ui-react";
 
 import "./styles.scss";
 
 /* eslint-disable */
 import Flex from "shared/flex";
-import Modal from "shared/modal";
-import { Money, Weights } from "shared/textFormatters";
 
 import ProductForm from "shop/productForm";
 import CartItemDetails from "shop/cartItemDetails";
 import CartDetails from "shop/cartDetails";
 
-import Context from "contexts/main";
+import withContext from "contexts/withContext";
 /* eslint-enable */
 
-const Wrapper = ({ cart, ...props }) => {
-    return (
-        <Context>
-            {ctx => (
-                <Cart {...props} cart={ctx.cart} loading={ctx.loading} userId={ctx.userId} getCtxData={ctx.getData} />
-            )}
-        </Context>
-    );
-};
-
-class Cart extends React.Component {
-    static propTypes = () => {
-        const { object } = PropTypes;
-        return {
-            cart: object
-        };
-    };
-    constructor(props) {
-        super(props);
-        this.state = {};
-    }
-
-    handleStickyRef = stickyRef => this.setState({ stickyRef });
+class Cart extends React.PureComponent {
 
     render() {
-        const { stickyRef } = this.state;
         const { cart } = this.props;
         const { cart_items: items = [], roaster_name = "" } = cart.attributes;
         return (
-            <div>
-                <div style={{ width: "calc( 100% - 328px )" }} ref={this.handleStickyRef}>
+            <Flex spacing="20" wrap>
+                <div flex="fill">
                     <Segment>
                         <Header as="h2" content={"Cart: " + roaster_name} dividing />
                         <Item.Group divided relaxed="very">
@@ -69,20 +44,18 @@ class Cart extends React.Component {
                                 );
                             })}
                         </Item.Group>
-                        <Rail position="right">
-                            <Sticky context={stickyRef} offset={100}>
-                                <CartDetails cart={cart} />
-                            </Sticky>
-                        </Rail>
                     </Segment>
                 </div>
-            </div>
+                <div className="cart-sidebar" flex="25">
+                    <CartDetails cart={cart} />
+                </div>  
+            </Flex>
         );
     }
 }
 
-Wrapper.propTypes = {
+Cart.propTypes = {
     cart: PropTypes.object
 };
 
-export default Wrapper;
+export default withContext(Cart);
