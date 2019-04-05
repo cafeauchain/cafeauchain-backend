2bd286b97cdb3a105106d536c6aa7213da668616
PendragonDevelopment <me@jordankburke.com>
2019-04-05 15:12:59 -0400

    Updated production dev

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,16 +79,16 @@ Rails.application.configure do
   config.active_support.deprecation = :notify
 
   config.action_mailer.smtp_settings = {
-    address: "smtp.mandrillapp.com",
+    address: "smtp.sendgrid.net",
     port: 587,
     domain: Rails.application.secrets.domain_name,
     authentication: "plain",
     enable_starttls_auto: true,
-    user_name: Rails.application.secrets.email_provider_username,
-    password: Rails.application.secrets.email_provider_apikey
+    user_name: "apikey",
+    password: Rails.application.credentials.production[:smtp_password]
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => Rails.application.secrets.domain_name }
+  config.action_mailer.default_url_options = { :host => Rails.application.credentials.production[:domain_name] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
