05e4266089742748eecf7d869060f7cc8179076b
Kyle Sullivan <ksullivan@roundsphere.com>
2019-07-05 22:51:16 -0400

    Remove unnecessary comma

--- db/migrate/20190705205343_setup_default_cutoff_times_for_all_roasters.rb
+++ db/migrate/20190705205343_setup_default_cutoff_times_for_all_roasters.rb
@@ -2,7 +2,7 @@ class SetupDefaultCutoffTimesForAllRoasters < ActiveRecord::Migration[5.2]
   def up
     RoasterProfile.all.each{|roaster|
       if roaster.cutoff.nil?
-        Cutoff.create(roaster_profile_id: roaster.id, day_1: '00:00', day_2: '00:00', day_3: '00:00', day_4: '00:00', day_5: '00:00', )
+        Cutoff.create(roaster_profile_id: roaster.id, day_1: '00:00', day_2: '00:00', day_3: '00:00', day_4: '00:00', day_5: '00:00')
       end
     }
   end
