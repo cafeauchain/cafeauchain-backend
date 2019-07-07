b9e2878677ab0235a8f4f4a66c0e4351ed5b4aa0
Kyle Sullivan <ksullivan@roundsphere.com>
2019-07-07 17:22:34 -0400

    Saved account info in development

--- app/javascript/components/roaster_onboarding/wholesaleSignup.js
+++ app/javascript/components/roaster_onboarding/wholesaleSignup.js
@@ -20,24 +20,24 @@ import withContext from "contexts/withContext";
 
 const defaults = {
     business: {
-        tax_id: "",
-        phone: "",
-        routing: "",
-        account: "",
-        account_confirm: ""
+        tax_id: process.env.NODE_ENV === "development" ? "000000000" : "",
+        phone: process.env.NODE_ENV === "development" ? "7061234567" : "",
+        routing: process.env.NODE_ENV === "development" ? "110000000" : "",
+        account: process.env.NODE_ENV === "development" ? "000123456789" : "",
+        account_confirm: process.env.NODE_ENV === "development" ? "000123456789" : ""
     },
     owner: {
         first_name: "",
         last_name: "",
         title: "",
         email: "",
-        phone: "",
+        phone: process.env.NODE_ENV === "development" ? "7061234567" : "",
         percent_ownership: "",
         ssn_last_4: "",
         dob: {
-            dob_day: "",
-            dob_month: "",
-            dob_year: ""
+            dob_day: process.env.NODE_ENV === "development" ? "09" : "",
+            dob_month: process.env.NODE_ENV === "development" ? "01" : "",
+            dob_year: process.env.NODE_ENV === "development" ? "1990" : ""
         },
         address: {
             street_1: "",
@@ -48,16 +48,16 @@ const defaults = {
         }
     },
     account_opener: {
-        first_name: "",
-        last_name: "",
-        title: "",
+        first_name: process.env.NODE_ENV === "development" ? "Jane" : "",
+        last_name: process.env.NODE_ENV === "development" ? "Doe" : "",
+        title: process.env.NODE_ENV === "development" ? "President" : "",
         email: "",
-        phone: "",
-        ssn_last_4: "",
+        phone: process.env.NODE_ENV === "development" ? "7061234567" : "",
+        ssn_last_4: process.env.NODE_ENV === "development" ? "1234" : "",
         dob: {
-            dob_day: "",
-            dob_month: "",
-            dob_year: ""
+            dob_day: process.env.NODE_ENV === "development" ? "09" : "",
+            dob_month: process.env.NODE_ENV === "development" ? "01" : "",
+            dob_year: process.env.NODE_ENV === "development" ? "1990" : ""
         },
         address: {
             street_1: "",
