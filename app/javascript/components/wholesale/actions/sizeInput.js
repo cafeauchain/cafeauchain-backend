18e7c43e44ba62b418a9fb06116508aea8bc8d3f
Kyle Sullivan <ksullivan@roundsphere.com>
2019-08-03 20:39:23 -0400

    Make SizeInput a function component

--- app/javascript/components/wholesale/actions/sizeInput.js
+++ app/javascript/components/wholesale/actions/sizeInput.js
@@ -7,45 +7,40 @@ import Input from "shared/input";
 import { Weights } from "shared/textFormatters";
 /* eslint-enable */
 
-class SizeInput extends React.Component {
-    state = {}
-    render(){
-        const { item, index, length, onChange, onRemove } = this.props;
-        return (
-            <React.Fragment>
-                <Input
-                    action
-                    name={item.id}
-                    label={"Size " + (index + 1) + " (in ounces)"}
-                    onChange={onChange}
-                    value={item.value}
-                    type="number"
-                    allowLP
-                >
-                    <input data-lpignore="true" />
+const SizeInput = ({ item, index, length, onChange, onRemove }) => (
+    <React.Fragment>
+        <Input
+            action
+            name={item.id}
+            label={"Size " + (index + 1) + " (in ounces)"}
+            onChange={onChange}
+            value={item.value}
+            type="number"
+            allowLP
+        >
+            <input data-lpignore="true" />
 
-                    <Button
-                        type="button"
-                        color="red"
-                        icon
-                        content={<Icon name="close" />}
-                        compact
-                        idx={index}
-                        onClick={onRemove}
-                        disabled={length < 2}
-                    />
-                </Input>
-                {Number(item.value) >= 16 && (
-                    <div style={{ margin: "-10px 0 10px" }}>
-                        <span>(Weight in lbs: </span>
-                        <Weights>{item.value === "" ? Number(0) : item.value}</Weights>
-                        <span>)</span>
-                    </div>
-                )}
-            </React.Fragment>
-        );
-    }
-}
+            <Button
+                type="button"
+                color="red"
+                icon
+                content={<Icon name="close" />}
+                compact
+                idx={index}
+                onClick={onRemove}
+                disabled={length < 2}
+            />
+        </Input>
+        {Number(item.value) >= 16 && (
+            <div style={{ margin: "-10px 0 10px" }}>
+                <span>(Weight in lbs: </span>
+                <Weights>{item.value === "" ? Number(0) : item.value}</Weights>
+                <span>)</span>
+            </div>
+        )}
+    </React.Fragment>
+);
+
 const { func, number, object } = PropTypes;
 SizeInput.propTypes = {
     item: object,
