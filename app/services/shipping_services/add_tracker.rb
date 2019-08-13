f8992bb54989a9622d23373f7fca989983fbb6fc
Kyle Sullivan <ksullivan@roundsphere.com>
2019-08-13 12:37:40 -0400

    Handle errors coming from EasyPost Tracker

--- app/services/shipping_services/add_tracker.rb
+++ app/services/shipping_services/add_tracker.rb
@@ -3,25 +3,16 @@ require 'easypost'
 module ShippingServices
   class AddTracker
 
-    def self.add(tracking_code, carrier, order_shipping_method)
+    def self.add(tracking_code, carrier)
         EasyPost.api_key = Rails.application.credentials[Rails.env.to_sym][:easypost_api_key]
-
-        tracker = EasyPost::Tracker.create({
-            tracking_code: tracking_code,
-            carrier: carrier
-        })
-
-        if tracker
-            order_shipping_method.update(
-                tracking_number: tracking_code, 
-                carrier: carrier, 
-                easypost_tracker_id: tracker[:id],
-                shipment_date: Time.now()
-            )
-            order_shipping_method.order.update(status: :shipped)
-            return true
-        else
-            return false
+        begin
+            tracker = EasyPost::Tracker.create({
+                tracking_code: tracking_code,
+                carrier: carrier
+            })
+            return tracker
+        rescue => e
+            return { message: e.message, status: e.http_status }
         end
     end
     
