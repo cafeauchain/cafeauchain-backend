00ad5a1fb4b0e7250ae62c28af9405d6060665ac
Kyle Sullivan <ksullivan@roundsphere.com>
2019-08-26 21:49:17 -0400

    Revert "Merge pull request #109 from cafeauchain/ks/print-invoice"
    This reverts commit f99c64b4a32f89f0d33b8488651c4e6696635656, reversing
    changes made to 17f20aa3efc7a48a9b515b82fa94aa2ab9489b07.

--- app/javascript/components/shop/order.js
+++ app/javascript/components/shop/order.js
@@ -1,20 +1,18 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Segment, Header, Label, Divider } from "semantic-ui-react";
-import moment from "moment";
 
 /* eslint-disable */
 import Flex from "shared/flex";
 import Table from "shared/table";
 
-import { sortBy, slugify } from "utilities";
+import { sortBy } from "utilities";
 
 import tableDefs from "defs/tables/orderLineItems";
 import Addresses from "wholesale/orders/partials/addresses";
 import Details from "wholesale/orders/partials/details";
 import PaymentDetails from "shop/orders/partials/payment_details";
 import Totals from "wholesale/orders/partials/totals";
-import Pdfer from "shop/orders/partials/pdfer";
 
 import withContext from "contexts/withContext";
 /* eslint-enable */
@@ -38,60 +36,41 @@ class Order extends React.PureComponent {
             color: invoice_status === "paid_in_full" ? "black" : "green"
         };
 
-        const order_date = moment(attributes.order_date).format("YYYY-MM-DD");
-        const roaster_name = slugify(roasterAtts.name);
-        const company_name = slugify(customerAtts.company_name)
-        const filename = order_date + "_" + "order_" + id + "_" + roaster_name + "_" + company_name;
-
         return (
             <div>
                 <Segment>
                     <Header as="h2" content="Order Details" dividing />
-                    <Flex spacebetween spacing="20">
-                        <div flex="auto">
-                            <a href="/shop/orders">Back to All Orders</a>
-                        </div>
-                        <div flex="auto">
-                            <Pdfer filename={filename} title="Download Invoice" />
-                        </div>
-                    </Flex>
+                    <a href="/shop/orders">Back to All Orders</a>
                     <Segment style={{ maxWidth: 900, margin: "40px auto" }}>
-                        <div id="pdf_container">
-                            <Label 
-                                size="large" 
-                                ribbon="right" 
-                                content={payment_label.text} 
-                                color={payment_label.color}
-                            />
-                            <Flex spacing="30" spacebetween>
-                                <div flex="66">
-                                    <Addresses roaster={roasterAtts} customer={customerAtts} />
-                                </div>
-                                <div flex="33" style={{ textAlign: "right" }}>
-                                    <Details attributes={attributes} id={id} isCustomer />
-                                    <Divider />
-                                    <PaymentDetails attributes={attributes} />
-                                </div>
-                            </Flex>
-                            <br />
-                            <Table tableDefs={tableDefs} data={sorted} />
-                            <br />
-                            <Flex spacing="30" spacebetween>
-                                <div flex="66">
-                                    {attributes.notes && (
-                                        <React.Fragment>
-                                            <p>
-                                                <strong>Order Notes:</strong>
-                                            </p>
-                                            <Segment secondary>{attributes.notes}</Segment>
-                                        </React.Fragment>
-                                    )}
-                                </div>
-                                <div flex="33" style={{ textAlign: "right", marginTop: "auto" }}>
-                                    <Totals attributes={attributes} isCustomer />
-                                </div>
-                            </Flex>
-                        </div>
+                        <Label size="large" ribbon="right" content={payment_label.text} color={payment_label.color} />
+                        <Flex spacing="30" spacebetween>
+                            <div flex="66">
+                                <Addresses roaster={roasterAtts} customer={customerAtts} />
+                            </div>
+                            <div flex="33" style={{ textAlign: "right" }}>
+                                <Details attributes={attributes} id={id} isCustomer />
+                                <Divider />
+                                <PaymentDetails attributes={attributes} />
+                            </div>
+                        </Flex>
+                        <br />
+                        <Table tableDefs={tableDefs} data={sorted} />
+                        <br />
+                        <Flex spacing="30" spacebetween>
+                            <div flex="66">
+                                {attributes.notes && (
+                                    <React.Fragment>
+                                        <p>
+                                            <strong>Order Notes:</strong>
+                                        </p>
+                                        <Segment secondary>{attributes.notes}</Segment>
+                                    </React.Fragment>
+                                )} 
+                            </div>
+                            <div flex="33" style={{ textAlign: "right", marginTop: "auto" }}>
+                                <Totals attributes={attributes} isCustomer />
+                            </div>
+                        </Flex>
                     </Segment>
                 </Segment>
             </div>
