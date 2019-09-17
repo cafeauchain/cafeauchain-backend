3ef8cc26169a413a7ae315adb734c9a33c2e0c42
Kyle Sullivan <ksullivan@roundsphere.com>
2019-09-16 22:05:07 -0400

    New production queue v1

--- app/controllers/manage/production_controller.rb
+++ app/controllers/manage/production_controller.rb
@@ -4,7 +4,7 @@ module Manage
 
     def index
       orders = current_user.roaster_profile.open_order_items
-      items = orders.group_by {|oi| oi[:size]}
+      items = orders.group_by{|o| o[:product]}.each_with_object({}) {|(k, v), h| h[k] = v.group_by{|i| i[:size]} }
       render "manage/primary", locals: {
         roaster: current_user.roaster_profile,
         orders: items,
