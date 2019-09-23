83b2b9c1d9d89f121db7ccfff90cfff9e07ed57a
Kyle Sullivan <ksullivan@roundsphere.com>
2019-09-23 12:45:46 -0400

    Revert "Merge pull request #144 from cafeauchain/testing-with-jest"
    This reverts commit edbcf55990d619faad973ef49a46c50d1e226d76, reversing
    changes made to dc29b0762a415edb1e0d0c2fc43cb8cab79b6ac0.

--- app/javascript/components/utilities/capitalize.test.js
+++
@@ -1,8 +0,0 @@
-import capitalize from "./capitalize";
-
-test('returns a capitalized string if given a string', () => {
-    expect(capitalize("TEstEd ME")).toBe("Tested me");
-    expect(capitalize("some String")).toBe("Some string");
-    expect(capitalize("THIS ONE TOO")).toBe("This one too");
-    expect(capitalize("last one")).toBe("Last one");
-});
