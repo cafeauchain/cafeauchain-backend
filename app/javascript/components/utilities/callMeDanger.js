0e8dcff88d01ab75186ac56f402b369adc941f03
Kyle Sullivan <ksullivan@roundsphere.com>
2019-09-23 12:00:30 -0400

    Dont capitalize callMeDanger

--- app/javascript/components/utilities/callMeDanger.js
+++ app/javascript/components/utilities/callMeDanger.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 // eslint-disable-next-line
-const CallMeDanger = (template, el = "span") =>
+const callMeDanger = (template, el = "span") =>
     React.createElement(el, { dangerouslySetInnerHTML: { __html: template } });
 
-export default CallMeDanger;
+export default callMeDanger;
